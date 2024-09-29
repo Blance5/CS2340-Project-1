ea1bd1785b308310ab2f2aab4740b19724f93daa
LRWilliams359 <rw03498@gmail.com>
2024-09-29 17:49:30 -0400

    FAvoriteButton

--- FoodFinder_Proj/FoodFinder/views.py
+++ FoodFinder_Proj/FoodFinder/views.py
@@ -1,6 +1,5 @@
 # home/views.py
-from django.http import HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from allauth.account.views import EmailView
 from allauth.account.views import LogoutView
 from django.views.generic import TemplateView
@@ -17,21 +16,26 @@ from .models import Restaurant
 # View for logged in users
 @login_required  # Ensures only logged in users can access this view
 def home_logged_in(request):
+    if request.method == 'POST':
+        place_id = request.POST['place_id']
+
+        # Check if the restaurant already exists
+        if Restaurant.objects.filter(place_id=place_id).exists():
+            restaurant = Restaurant.objects.get(place_id=place_id)
+            if restaurant.favorites.filter(id=request.user.id).exists():
+                restaurant.favorites.remove(request.user)
+            else:
+                restaurant.favorites.add(request.user)
+        else:
+            name = request.POST['name']
+            rating = request.POST['rating']
+            newrestaurant = Restaurant.objects.create(place_id=place_id, name=name, rating=rating)
+            newrestaurant.favorites.add(request.user)
     return render(request, 'home_logged_in.html')
-
-def add_favorite(request, place_id):
-    restaurant = get_object_or_404(Restaurant, id=place_id)
-
-    if restaurant.favorites.filter(id=request.user.id).exists():
-        restaurant.favorites.remove(request.user)
-    else:
-        restaurant.favorites.add(request.user)
-
-    return redirect('home_logged_in')
 
 @login_required
 def profile_view(request):
-    fav_restaurant = Favorite.objects.filter(favorites=request.user)
+    fav_restaurant = Restaurant.objects.filter(favorites=request.user)
     context = {
         'fav_restaurant': fav_restaurant,
         'user': request.user
