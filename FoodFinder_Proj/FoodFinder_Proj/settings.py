9fd18ce1a9c923a69366176b2b2dd922936e1066
BenHl <benhlance@gmail.com>
2024-11-05 19:43:04 -0500

    Update settings.py

--- FoodFinder_Proj/FoodFinder_Proj/settings.py
+++ FoodFinder_Proj/FoodFinder_Proj/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'FoodFinder_Proj.FoodFinder',
+    'FoodFinder',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
