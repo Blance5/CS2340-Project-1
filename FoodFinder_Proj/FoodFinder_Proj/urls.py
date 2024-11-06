cccd85180f5b28c32d61050e2a03defb93261fdd
BenHl <benhlance@gmail.com>
2024-11-05 19:54:49 -0500

    Update urls.py

--- FoodFinder_Proj/FoodFinder_Proj/urls.py
+++ FoodFinder_Proj/FoodFinder_Proj/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from FoodFinder_Proj.FoodFinder.views import home_view
+from FoodFinder_Proj.FoodFinder.views import home_redirect
 from FoodFinder.views import CustomLogoutView  # Import the custom logout view
 
 urlpatterns = [
@@ -10,7 +10,7 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),  # Include allauth URLs for authentication
     path('accounts/logout/', CustomLogoutView.as_view(), name='account_logout'),  # Custom logout view
     path('profile/', profile_view, name='profile'),  # New profile URL
-    path('', home_view, name='home'),  # Home page view
+    path('', home_redirect, name='home'),  # Home page view
 
 ]
 
